e012016ccbc1e6b829bee93c4d615eab40873c3e
Muhammed Hazim Abhar bin Mohamed Azahar <hazimabhar123@gmail.com>
2023-06-17 14:57:27 +0800

    update

--- index.js
+++ index.js
@@ -633,19 +633,24 @@ app.get("/report/today", async (req, res) => {
   const startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
   const endDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
 
+  // Adjust start and end dates to match the format of the 'createdAt' field
+  const startDateFormatted = startDate.toISOString().split('T')[0] + ' 00:00:00';
+  const endDateFormatted = endDate.toISOString().split('T')[0] + ' 00:00:00';
+
   const todayReport = await prisma.report.findMany({
     where: {
       createdAt: {
-        gte: startDate.toISOString(),
-        lt: endDate.toISOString(),
+        gte: startDateFormatted,
+        lt: endDateFormatted,
       },
     },
   });
 
-  console.log(today),
-  console.log(startDate)
-  console.log(endDate)
-  console.log(todayReport)
+  console.log(today);
+  console.log(startDate);
+  console.log(endDate);
+  console.log(todayReport);
+  
   res.json(todayReport);
 });
 
