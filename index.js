dc41fdb1f9ee6b1f4b994f454e88e34b81dc59d0
Muhammed Hazim Abhar bin Mohamed Azahar <hazimabhar123@gmail.com>
2023-07-17 22:26:41 +0800

    update

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const cron = require('node-cron');
 let loginAttempt =0
 
 
-app.use(cors({
-  origin:'*'
-}))
-app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
- 
 //crud user
 app.get("/", async (req, res)=>{
 
