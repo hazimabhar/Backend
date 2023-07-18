005e11c8e29b1e7332dd7b225fb76334f63a850c
Muhammed Hazim Abhar bin Mohamed Azahar <hazimabhar123@gmail.com>
2023-07-18 11:07:08 +0800

    update

--- index.js
+++ index.js
@@ -962,7 +962,7 @@ async function generateReport(){
   return createdReport
 }
 
-cron.schedule('30 0 * * *', async () => {
+cron.schedule('10 3 * * *', async () => {
   try { 
     const report = await generateReport()
   } catch (error) {
